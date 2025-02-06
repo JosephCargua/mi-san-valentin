7a39fd3ba3efcbf86c763d2da6ef6ed6ad214da5
JosephCargua <josepcargua94@gmail.com>
2025-02-06 01:19:44 -0500

    Correccion frases

--- src/inicio.jsx
+++ src/inicio.jsx
@@ -15,10 +15,10 @@ const Reasons = () => {
             <h1><span className="number">5</span> <span className="red">Razones</span> <br/> por las que eres <br/> <span className="cursive">Mi Elección Perfecta</span></h1>
 
             <ul className="reasons">
-                <li><span className="num">1</span> Tienes la sonrisa más linda del universo</li>
-                <li><span className="num">2</span> Contigo, cualquier día se siente especial</li>
-                <li><span className="num">3</span> Haces que mi corazón lata más rápido cada vez que te veo</li>
-                <li><span className="num">4</span> Eres mi persona favorita para compartir momentos</li>
+                <li><span className="num">1</span> Tiene la sonrisa más linda del universo</li>
+                <li><span className="num">2</span> Con usted, cualquier día se siente especial</li>
+                <li><span className="num">3</span> Hace que mi corazón lata más rápido cada vez que le veo</li>
+                <li><span className="num">4</span> Es mi persona favorita para compartir momentos</li>
                 <li><span className="num">5</span> No imagino a nadie más a mi lado en este San Valentín</li>
             </ul>
 
